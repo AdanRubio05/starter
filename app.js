432d864478e2d04aec66be34a617234f37315e94
AdanRubio05 <111942322+AdanRubio05@users.noreply.github.com>
2024-11-04 17:22:20 -0600

    js added
    'date' should be 'data'

---
+++ app.js
@@ -0,0 +1,36 @@
+let date = [
+    {
+        name: 'Frankenstein',
+        age: '206'  
+    },
+    {
+        name: 'Dracula',
+        age: '127'  
+    }, 
+    {
+        name: 'Griffin',
+        age: '127'  
+    },
+    {
+        name: 'Mr. Hyde',
+        age: '138'  
+    },
+    {
+        name: 'King Kong',
+        age: '91'  
+    },
+    {
+        name: 'Gojira',
+        age: '70'  
+    }
+] ;
+
+const info = document.querySelector('#info')
+
+let details = data.map(function(){
+    return (
+        '<div>' + item.name + '</div>'
+    )
+})
+
+info.innerHTML = details.join('\n') ;
